422cfe652ff5d43af21eeb3d9428cdd966911763
burkeperrotta <114622569+burkeperrotta@users.noreply.github.com>
2023-10-15 23:27:52 -0400

    2023-10-15 BP HW 5a

--- hw4a/github_api.py
+++ hw4a/github_api.py
@@ -1,12 +1,10 @@
 import requests
 
 def get_user_repositories_and_commits(username):
-    try:
         # Get the user's repositories
         repo_url = f'https://api.github.com/users/{username}/repos'
         print(repo_url)
         response = requests.get(repo_url)
-        response.raise_for_status()  # Raise an exception if the request was unsuccessful
         repos = response.json()
 
         repo_commits = []  # Initialize a list to store repo and commit count pairs
@@ -18,19 +16,13 @@ def get_user_repositories_and_commits(username):
             # Get the commits for each repository
             commit_url = f'https://api.github.com/repos/{username}/{repo_name}/commits'
             commit_response = requests.get(commit_url)
-            commit_response.raise_for_status()  # Raise an exception if the request was unsuccessful
             commits = commit_response.json()
             commit_count = len(commits)
 
             repo_commits.append((repo_name, commit_count))
 
-        # Print the result
-        for repo, commit_count in repo_commits:
-            print(f"The repo {repo} has {commit_count} commits.")
         return repo_commits
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data from GitHub API: {e}")
-        return None
+
 
 # uName = input("Enter the username of a github you want to see info for: ")
 # get_user_repositories_and_commits(uName)
--- hw4a/test_github_api.py
+++ hw4a/test_github_api.py
@@ -1,31 +1,47 @@
 import unittest
+from unittest.mock import patch, MagicMock
 from github_api import get_user_repositories_and_commits
 
 class TestGitHubAPI(unittest.TestCase):
 
-    def test_get_user_repositories_and_commits(self):
-        username = "richkempinski"
+    @patch('requests.get')  # mock the get function
+    def test_get_user_repositories_and_commits(self, mock_get):
+
+        # make repository response
+        mock_response_repos = MagicMock()
+        mock_response_repos.json.return_value = [{'name': 'repo1'}, {'name': 'repo2'}, {'name': 'repo3'}, {'name': 'repo4'}, {'name': 'repo5'}, 
+                                                 {'name': 'repo6'}, {'name': 'repo7'}, {'name': 'repo8'}, {'name': 'repo9'}]
+        # make commit response
+        mock_response_commits = MagicMock()
+        mock_response_commits.json.return_value = [{'commit1'}, {'commit2'}]
+
+
+        mock_get.side_effect = [mock_response_repos, mock_response_commits, mock_response_commits, mock_response_commits, mock_response_commits, 
+                                mock_response_commits, mock_response_commits, mock_response_commits, mock_response_commits, mock_response_commits]
+
+        username = "example_username"
         repo_commits = get_user_repositories_and_commits(username)
+        
 
-        #initial test to make sure function returns a result.
+        # test theat the result isnt empty
         self.assertIsNotNone(repo_commits)
-        # test if the number of repos is correct
-        self.assertEqual(len(repo_commits), 9)
-        # test all of th eexample user's repos and commit numbers
-        self.assertEqual(repo_commits[0], ('csp', 2))
-        self.assertEqual(repo_commits[1], ('hellogitworld', 30))
-        self.assertEqual(repo_commits[2], ('helloworld', 6))
-        self.assertEqual(repo_commits[3], ('Mocks', 10))
-        self.assertEqual(repo_commits[4], ('Project1', 2))
-        self.assertEqual(repo_commits[5], ('richkempinski.github.io', 9))
-        self.assertEqual(repo_commits[6], ('threads-of-life', 1))
-        self.assertEqual(repo_commits[7], ('try_nbdev', 2))
-        self.assertEqual(repo_commits[8], ('try_nbdev2', 5))
-        #test an invalid username
-        invalName = "njkcdosnjfionreuiwofnusdiovnf7"
-        invalNameTest = get_user_repositories_and_commits(invalName)
-        self.assertEqual(invalNameTest, (None))
 
+        # test that the result is the correct length
+        self.assertEqual(len(repo_commits), 9)
+
+
+        # test the different values in the result
+        self.assertEqual(repo_commits[0], ('repo1', 2))
+        self.assertEqual(repo_commits[1], ('repo2', 2))
+        self.assertEqual(repo_commits[2], ('repo3', 2))
+        self.assertEqual(repo_commits[3], ('repo4', 2))
+        self.assertEqual(repo_commits[4], ('repo5', 2))
+        self.assertEqual(repo_commits[5], ('repo6', 2))
+        self.assertEqual(repo_commits[6], ('repo7', 2))
+        self.assertEqual(repo_commits[7], ('repo8', 2))
+        self.assertEqual(repo_commits[8], ('repo9', 2))
 
 if __name__ == '__main__':
     unittest.main()
+
+
